5176b4396e5eb72e417401dd337fe7a4847c4576
maguiat <magaliat16@gmail.com>
2025-10-14 16:28:44 -0300

    feat(model): implementar modelos Album y Cancion con atributos y restricciones

--- src/models/Album.js
+++ src/models/Album.js
@@ -1,4 +1,42 @@
-/**
- * Modelo Album
- * Los estudiantes deben implementar todas las operaciones CRUD para álbumes
- */
+const { DataTypes } = require('sequelize')
+const sequelize= require('../config/database')
+
+const Album = sequelize.define('album', {
+    id_album: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    titulo: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true,
+    },
+    // FK
+    id_artista: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    // FK
+    id_discografica: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    imagen_portada: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    anio_publicacion: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+    },
+    duracion_total_seg: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+}, {
+    tableName: 'album',
+    timestamps: false
+})
+
+module.exports = Album
--- src/models/Cancion.js
+++ src/models/Cancion.js
@@ -1,4 +1,41 @@
-/**
- * Modelo Cancion
- * Los estudiantes deben implementar todas las operaciones CRUD para canciones
- */
+const { DataTypes } = require('sequelize')
+const sequelize= require('../config/database')
+
+const Cancion = sequelize.define('cancion', {
+    id_cancion: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    titulo: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    duracion_seg: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    // FK
+    id_album: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    reproducciones: {
+        type: DataTypes.BIGINT,
+        defaultValue: 0,
+    },
+    likes: {
+        type: DataTypes.BIGINT,
+        defaultValue: 0,
+    },
+    fecha_agregada: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false,
+    },
+}, {
+    tableName: 'cancion',
+    timestamps: false
+})
+
+module.exports = Cancion
