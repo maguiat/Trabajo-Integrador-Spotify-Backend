e60171cc761ea84b06d469d021262dfc748142d9
maguiat <magaliat16@gmail.com>
2025-10-14 16:15:01 -0300

    feat(model): implementar modelo Artista con atributos y restricciones

--- src/models/Artista.js
+++ src/models/Artista.js
@@ -1,4 +1,24 @@
-/**
- * Modelo Artista
- * Los estudiantes deben implementar todas las operaciones CRUD para artistas
- */
+const { DataTypes } = require('sequelize')
+const sequelize= require('../config/database')
+
+const Artista = sequelize.define('artista', {
+    id_artista: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nombre: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true,
+    },
+    imagen_url: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+}, {
+    tableName: 'artista',
+    timestamps: false
+})
+
+module.exports = Artista
