0d21fda12e28226d512f209ffc019dfceca3a7d6
maguiat <magaliat16@gmail.com>
2025-10-14 16:35:56 -0300

    feat(model): implementar modelo Genero y Playlist con atributos y restricciones

--- src/models/Genero.js
+++ src/models/Genero.js
@@ -1,4 +1,20 @@
-/**
- * Modelo Genero
- * Los estudiantes deben implementar todas las operaciones CRUD para géneros
- */
+const { DataTypes } = require('sequelize')
+const sequelize= require('../config/database')
+
+const Genero = sequelize.define('genero', {
+    id_genero: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nombre: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true,
+    },
+}, {
+    tableName: 'genero',
+    timestamps: false
+})
+
+module.exports = Genero
--- src/models/Playlist.js
+++ src/models/Playlist.js
@@ -2,3 +2,45 @@
  * Modelo Playlist
  * Los estudiantes deben implementar todas las operaciones CRUD para playlists
  */
+
+const { DataTypes } = require("sequelize")
+const sequelize = require("../config/database")
+
+const Playlist = sequelize.define("playlist", {
+  id_playlist: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  titulo: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  // FK
+  id_usuario: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  cant_canciones: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  estado: {
+    type: DataTypes.ENUM("Activa", "Eliminada"),
+    defaultValue: "Activa",
+  },
+  fecha_creacion: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    allowNull: false,
+  },
+  fecha_eliminada: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+}, {
+  tableName: "playlist",
+  timestamps: false,
+})
+
+module.exports = Playlist
