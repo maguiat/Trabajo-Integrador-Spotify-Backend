a2cf58c85c5e78d9bb45d0c56b9163f979ea63e9
maguiat <magaliat16@gmail.com>
2025-10-19 15:49:45 -0300

    refactor(rutas): reorganizar rutas de usuarios para mejorar la legibilidad

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -4,10 +4,20 @@ const usuariosController = require("../controllers/usuariosController")
 
 // Ruta para listar usuarios
 router.get("/", usuariosController.getAllUsuarios)
+
+// Ruta para obtener un usuario por ID
 router.get("/:id", usuariosController.getUsuarioByID)
+
+// Ruta para crear un usuario
 router.post("/", usuariosController.crearUsuario)
+
+// Ruta para actualizar un usuario
 router.put("/:id", usuariosController.updateUsuario)
+
+// Ruta para eliminar un usuario
 router.delete("/:id", usuariosController.deleteUsuario)
+
+// Ruta para obtener usuarios con password vencidas
 router.get("/password-vencidas", usuariosController.getUsuariosPasswordVencidas)
 
 module.exports = router
